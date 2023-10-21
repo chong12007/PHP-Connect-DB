fb830841eb9dfedd05ce3313f5360fa23dbc7647
chong12007 <chong12007@gmail.com>
2023-10-21 15:12:36 +0800

    Update connect-database.php

--- Pratical_4/connect-database.php
+++ Pratical_4/connect-database.php
@@ -12,7 +12,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }else{
-    echo 'yeah';
+    echo 'Connected to database';
 }
 
 function getGenders() {
